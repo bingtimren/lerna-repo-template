9bf5fcfb4c949bbab5b06e238c885c135fed1cf5
Bing Ren <bingtimren@gmail.com>
2021-06-25 12:34:01 +1000

    refactor: change awscdk file structure and tools

--- apps/awscdk/src/cdk/app/awscdk.ts
+++ apps/awscdk/src/cdk/app/awscdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-import { AwscdkStack } from "../lib/awscdk-stack";
+import { AwscdkStack } from "../stacks/awscdk-stack";
 
 const app = new cdk.App();
 new AwscdkStack(app, "AwscdkStack", {
--- apps/awscdk/test/awscdk.test.ts
+++ apps/awscdk/test/awscdk.test.ts
@@ -4,7 +4,7 @@ import {
   MatchStyle,
 } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
-import * as Awscdk from "../lib/awscdk-stack";
+import * as Awscdk from "../src/cdk/stacks/awscdk-stack";
 
 test("Empty Stack", () => {
   const app = new cdk.App();
